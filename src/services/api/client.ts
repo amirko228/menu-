c7060a14c91321eec1e729b49dfb4450342fc965
amir <ogonbashevamir@gmail.com>
2026-01-10 14:12:33 +0600

    Fix baseURL TypeScript error

--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -5,45 +5,49 @@ import { ApiClient, ApiResponse } from './types';
  * Готов к замене на реальный HTTP клиент (axios, fetch и т.д.)
  */
 class BaseApiClient implements ApiClient {
-  private readonly baseURL: string;
+  private readonly _baseURL: string;
 
   constructor(baseURL: string = '/api') {
-    this.baseURL = baseURL;
+    this._baseURL = baseURL;
   }
 
   async get<T>(_url: string): Promise<ApiResponse<T>> {
     // TODO: Заменить на реальный HTTP запрос
-    // const response = await fetch(`${this.baseURL}${_url}`);
+    // const response = await fetch(`${this._baseURL}${_url}`);
     // return response.json();
+    void this._baseURL; // Зарезервировано для будущего использования
     throw new Error('API client not implemented. Use repositories with LocalStorage.');
   }
 
   async post<T>(_url: string, _data?: unknown): Promise<ApiResponse<T>> {
     // TODO: Заменить на реальный HTTP запрос
-    // const response = await fetch(`${this.baseURL}${_url}`, {
+    // const response = await fetch(`${this._baseURL}${_url}`, {
     //   method: 'POST',
     //   body: JSON.stringify(_data),
     // });
     // return response.json();
+    void this._baseURL; // Зарезервировано для будущего использования
     throw new Error('API client not implemented. Use repositories with LocalStorage.');
   }
 
   async put<T>(_url: string, _data?: unknown): Promise<ApiResponse<T>> {
     // TODO: Заменить на реальный HTTP запрос
-    // const response = await fetch(`${this.baseURL}${_url}`, {
+    // const response = await fetch(`${this._baseURL}${_url}`, {
     //   method: 'PUT',
     //   body: JSON.stringify(_data),
     // });
     // return response.json();
+    void this._baseURL; // Зарезервировано для будущего использования
     throw new Error('API client not implemented. Use LocalStorage.');
   }
 
   async delete<T>(_url: string): Promise<ApiResponse<T>> {
     // TODO: Заменить на реальный HTTP запрос
-    // const response = await fetch(`${this.baseURL}${_url}`, {
+    // const response = await fetch(`${this._baseURL}${_url}`, {
     //   method: 'DELETE',
     // });
     // return response.json();
+    void this._baseURL; // Зарезервировано для будущего использования
     throw new Error('API client not implemented. Use LocalStorage.');
   }
 }
